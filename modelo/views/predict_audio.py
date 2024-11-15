788ab7e575c172f24065a9b1e1f14608c057bff5
wicher_leonardo <leonardo.wlferreira@gmail.com>
2024-11-15 20:05:21 -0300

    API do Modelo

--- modelo/views/predict_audio.py
+++ modelo/views/predict_audio.py
@@ -1,4 +1,5 @@
 # modelo/views/predict_audio
+import asyncio
 import tensorflow as tf
 import numpy as np
 from .audio_to_spectrogram import audio_to_spectrogram
@@ -11,13 +12,13 @@ import base64
 # Rodando o modelo toda vez que api for iniciada
 model = tf.keras.models.load_model('./modelo/modelo_sirene_v1.0.1.h5')
 
-def audio_to_base64(file_path):
+async def audio_to_base64(file_path):
     """Converte o arquivo de áudio para base64"""
     with open(file_path, "rb") as audio_file:
         return base64.b64encode(audio_file.read()).decode('utf-8')
 
 # Função para fazer previsões com o modelo carregado
-def predict_audio(file_path):
+async def predict_audio(file_path):
     try:
         if not file_path.lower().endswith(('.wav', '.mp3')):
             return {"error": "Arquivo não é de um formato de áudio válido."}
@@ -25,10 +26,11 @@ def predict_audio(file_path):
         start_time = time.time()
 
         # Converte o áudio em base64
-        audio_base64 = audio_to_base64(file_path)
+        audio_base64 = await asyncio.to_thread(audio_to_base64,file_path)
 
-        spectrogram = audio_to_spectrogram(file_path)
-        prediction = model.predict(spectrogram)
+        spectrogram = await asyncio.to_thread(audio_to_spectrogram, file_path)
+
+        prediction = await asyncio.to_thread(model.predict, spectrogram)
 
         end_time = time.time()
         tempo_resposta = end_time - start_time
@@ -38,14 +40,14 @@ def predict_audio(file_path):
         predicted_class = classes[np.argmax(prediction)]
 
         # Criar imagens e obter base64
-        spectrogram_path, spectrogram_base64 = create_spectrogram_image(file_path)
-        waveform_path, waveform_base64 = create_waveform_image(file_path)
+        spectrogram_path, spectrogram_base64 = await asyncio.to_thread(create_spectrogram_image, file_path)
+        waveform_path, waveform_base64 = await asyncio.to_thread(create_waveform_image, file_path)
 
         # Simular vetor do áudio convertido (adapte para seu caso)
         # audio_vector = spectrogram.flatten().tolist()
 
         try:
-            saved_id = save_prediction_to_db(
+            saved_id = await asyncio.to_thread(save_prediction_to_db,
                 predicted_class,
                 tempo_resposta,
                 file_path.split('/')[-1],
--- routes/ia_data_routes.py
+++ routes/ia_data_routes.py
@@ -74,23 +74,3 @@ def get_data_by_id(id):
         return jsonify({"error": str(e)}), 400
 
 
-# @ia_data_blue_print.route('/create_data', methods=['POST'])
-# def create_data():
-#     data = request.get_json()
-#     try:
-#         # Converter strings de data e hora para objetos datetime
-#         data_identificacao = datetime.strptime(data['data_identificacao'], '%Y-%m-%d').date()
-#         horario_identificacao = datetime.strptime(data['horario_identificacao'], '%Y-%m-%d %H:%M:%S')
-
-#         novo_dado = IAData(
-#             tipo_ruido=data['tipo_ruido'],
-#             data_identificacao=data_identificacao,
-#             horario_identificacao=horario_identificacao,
-#             tempo_resposta=data['tempo_resposta']
-#         )
-
-#         novo_dado.save()
-
-#         return jsonify({"message": "Dado inserido com sucesso!", "data": data}), 201
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 400
